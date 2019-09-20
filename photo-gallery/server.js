d7dc23dd2610e095ff13ac07199f17ef8559aa80
Вика <kusyshar@gmail.com>
2019-09-20 13:49:58 +0400

    heroku fix

--- photo-gallery/server.js
+++ photo-gallery/server.js
@@ -14,4 +14,5 @@ res.sendFile(path.join(__dirname +'/dist/photo-gallery/index.html'));
 
 
 // Start the app by listening on the default Heroku port 
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
+app.listen(3001);
