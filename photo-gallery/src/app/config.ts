320a3bc65834a4060aa7cf60f24127123fc7c2e4
Вика <kusyshar@gmail.com>
2019-12-20 03:25:09 +0400

    true way

--- photo-gallery/src/app/config.ts
+++ photo-gallery/src/app/config.ts
@@ -1,6 +1,6 @@
 // const way = 'http://localhost:3001';
 // const wayWS = 'ws://localhost:3002';
-const way = 'https://photogalleryvikanpm .herokuapp.com';
+const way = 'https://photogalleryback.herokuapp.com';
 const wayWS = 'wss://photogalleryws.herokuapp.com';
 export {
     way,
